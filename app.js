c10e599dc750e77df7cfad3955aea65c73a7d6d5
Lucas8353 <lucas531@hotmail.com>
2022-04-13 20:31:50 -0300

    modificacion app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static("./public"));
 
-app.listen(process.env.PORT || 3030, ()=> 
+app.listen(process.env.PORT || 3030, function()=> 
     console.log("esta funcionando http://localhost:3030")
 );
 
